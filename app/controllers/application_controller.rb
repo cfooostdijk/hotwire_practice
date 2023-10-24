79f46fddd269cdd9b03c2d836b4130e8338a0d9c
CrisOos <cristian.oostdijk@avenida.com>
2023-10-23 23:41:34 -0300

    Turbo Drive

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action -> { sleep 2 }
+  def page1; end
+
+  def page2; end
+
+  def page3
+    redirect_to page2_path, status: :see_other
+  end
 end
